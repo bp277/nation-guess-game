10b361de9fcf84913197ea1645a5a1730502d481
bp277 <bosko277@gmail.com>
2022-08-06 20:18:24 +0200

    v1

---
+++ js/answer.js
@@ -0,0 +1,9 @@
+export class Answer {
+    constructor(flag, continent,population,landSize,borderingCountries) {
+        this.flag = flag;
+        this.continent = continent;
+        this.population = population;
+        this.landSize = landSize;
+        this.borderingCountries = borderingCountries;
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,64 +1,208 @@
-import { countries } from "./countries.js";
+import { unMembers } from "./countries.js";
+import { Answer } from "./answer.js";
 
-const main = document.querySelector('main');
+const main = document.querySelector("main");
 const input = document.querySelector("#input");
 const answers = document.querySelector(".answers");
-const searchList = document.querySelector(".search-list")
+const searchList = document.querySelector(".search-list");
+const listItem = document.querySelector(".list-item");
+const backdrop = document.querySelector(".backdrop");
+const backdropCoa = document.querySelector(".backdrop-coa");
+const backdropFlag = document.querySelector(".backdrop-flag");
+const correctName = document.querySelector(".correct-name");
+const correctFlag = document.querySelector(".correct-flag");
+const newGame = document.querySelector(".new-game");
+const coatsOfArms = document.querySelector(".coa")
+const hintFlag = document.querySelector(".hint-flag")
+const imgCoa = document.querySelector(".img-coa")
+const modalFlag = document.querySelector(".img-flag")
+
+
+
+let countryInfo = [];
+for (let i = 0; i < unMembers.length; i++) {
+  countryInfo.push([
+    unMembers[i].flags.png,
+    unMembers[i].name.common,
+    unMembers[i].continents,
+    unMembers[i].population,
+    unMembers[i].area,
+    unMembers[i].borders == undefined
+      ? (unMembers[i].borders = [])
+      : unMembers[i].borders,
+    unMembers[i].coatOfArms.png
+  ]);
+}
 
 //Searching and Submiting answer
 input.addEventListener("keyup", () => {
   // Creating list of search results
-  const searchItem = document.createElement("li")
+  const searchItem = document.createElement("li");
 
-  const inputValue = input.value
-  const filtered = countries.filter((c) => (c["country"].toLowerCase()).startsWith(inputValue.toLowerCase()));
-  const searchResult = (Object.values(filtered).map((obj) => {
-    return obj.country
-  }));
-  searchResult.sort()
+  const inputValue = input.value;
+
+  const filtered = countryInfo.filter((c) =>
+    c[1].toLowerCase().includes(inputValue.toLowerCase())
+  );
+  const searchResult = Object.values(filtered).map((obj) => {
+    return obj[1];
+  });
+  searchResult.sort();
   if (searchResult) {
     searchList.innerHTML = "";
     let innerElement = "";
     searchResult.forEach((item) => {
-      innerElement += `<li>${item}</li>`
-    })
-    searchList.innerHTML = innerElement
+      innerElement += `<li class="list-item">${item}</li>`;
+    });
+    searchList.innerHTML = innerElement;
   }
-  })
-  
+});
 
 input.addEventListener("keyup", (e) => {
   if (e.code === "Enter") {
-
     createAnswer();
   }
 });
 
+function selectCountry(e) {
+  for (let i = 0; i < countryInfo.length; i++) {
+    if (e.target.innerHTML === unMembers[i].name.common) {
+      new Answer(
+        unMembers[i].flags.png,
+        unMembers[i].continents[0],
+        unMembers[i].population,
+        unMembers[i].area,
+        unMembers[i].borders.length
+      );
+
+      createAnswer(
+        unMembers[i].flags.png,
+        unMembers[i].continents[0],
+        unMembers[i].population
+          .toString()
+          .replace(/\B(?=(\d{3})+(?!\d))/g, ","),
+        unMembers[i].area.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",") + " km²",
+        unMembers[i].borders.length
+      );
+
+      // Open modal and end the game when answer is correct or when the user exceeds the limited number of guesses
+      if (
+        e.target.innerHTML === correctAnswer[1] ||
+        answers.childNodes.length == 8
+      ) {
+        console.log("Correct!");
+        backdrop.style.display = "flex";
+        correctName.textContent = correctAnswer[1];
+        correctFlag.src = correctAnswer[0];
+      }
+    }
+    input.value = "";
+    searchList.innerHTML = "";
+  }
+}
+newGame.addEventListener("click", () => location.reload());
+
+if (searchList) {
+  searchList.addEventListener("click", selectCountry);
+}
 //Creating and appending the answer
 
-function createAnswer() {
+function createAnswer(flg, cont, pop, siz, bord) {
   const answer = document.createElement("div");
   answer.classList.add("answer");
   answers.appendChild(answer);
 
-  // Creating answers elements
   const flag = document.createElement("div");
   flag.classList.add("flag");
+  answer.appendChild(flag);
+  const flagImg = document.createElement("img");
+  flag.appendChild(flagImg);
+  flagImg.src = flg;
+  flagImg.setAttribute("width", "50%");
+  flagImg.setAttribute("height", "50%");
+
   const continent = document.createElement("div");
   continent.classList.add("continent");
+  answer.appendChild(continent);
+  const continentPara = document.createElement("p");
+  continent.appendChild(continentPara);
+  continentPara.textContent = cont;
+
   const population = document.createElement("div");
   population.classList.add("population");
-  const landSize = document.createElement("div");
-  landSize.classList.add("landSize");
-  const borderingCountries = document.createElement("div");
-  borderingCountries.classList.add("borderingCountries");
-
-  //Appending elements to answer div
-  answer.appendChild(flag);
-  answer.appendChild(continent);
   answer.appendChild(population);
-  answer.appendChild(landSize);
-  answer.appendChild(borderingCountries);
+  const populationPara = document.createElement("p");
+  population.appendChild(populationPara);
+  populationPara.textContent = pop;
 
-  //
+  const size = document.createElement("div");
+  size.classList.add("size");
+  answer.appendChild(size);
+  const sizePara = document.createElement("p");
+  size.appendChild(sizePara);
+  sizePara.textContent = siz;
+
+  const borders = document.createElement("div");
+  borders.classList.add("borders");
+  answer.appendChild(borders);
+  const bordersPara = document.createElement("p");
+  borders.appendChild(bordersPara);
+  bordersPara.textContent = bord;
+
+
+
+  if (flagImg.src == correctAnswer[0]){
+    flag.classList.add("success")
+  }
+  if (continentPara.innerHTML === correctAnswer[2][0]) {
+    continent.classList.add("success");
+  }
+  if (
+    parseFloat(populationPara.innerHTML.replace(/,/g, "")) < correctAnswer[3]
+  ) {
+    population.classList.add("higher");
+  } else if (
+    parseFloat(populationPara.innerHTML.replace(/,/g, "")) > correctAnswer[3]
+  ) {
+    population.classList.add("lower");
+  } else if (
+    parseFloat(populationPara.innerHTML.replace(/,/g, "")) == correctAnswer[3]
+  ) {
+    population.classList.add("success");
+  }
+  if (parseFloat(sizePara.innerHTML.replace(/,/g, "")) < correctAnswer[4]) {
+    size.classList.add("higher");
+  } else if (
+    parseFloat(sizePara.innerHTML.replace(/,/g, "")) > correctAnswer[4]
+  ) {
+    size.classList.add("lower");
+  } else if (parseFloat(sizePara.innerHTML.replace(/,/g, "")) == correctAnswer[4]) {
+    size.classList.add("success");
+  }
+  if (parseInt(bordersPara.innerHTML) > correctAnswer[5].length) {
+    borders.classList.add("lower");
+  } else if (parseInt(bordersPara.innerHTML) < correctAnswer[5].length) {
+    borders.classList.add("higher");
+  } else if (parseInt(bordersPara.innerHTML) == correctAnswer[5].length) {
+    borders.classList.add("success");
+  }
 }
+//Show Coats Of Arms
+coatsOfArms.addEventListener("click", () => {
+  backdropCoa.style.display = "flex"
+})
+backdropCoa.addEventListener("click", () => {
+  backdropCoa.style.display = "none"
+})
+//Show Flag
+hintFlag.addEventListener("click", () => {
+  backdropFlag.style.display = "flex"
+})
+backdropFlag.addEventListener("click", () => {
+  backdropFlag.style.display = "none"
+})
+
+let correctAnswer = countryInfo[Math.floor(Math.random() * countryInfo.length)];
+imgCoa.src = correctAnswer[6];
+modalFlag.src = correctAnswer[0];
+
