48e48c9ddc158d3c5724b73c5e341dfe14023fe9
bp277 <bpejic27@gmail.com>
2023-01-27 00:05:47 +0100

    fix: End game modal not closing on click

--- js/main.js
+++ js/main.js
@@ -27,8 +27,8 @@ const allCountries = fetchedCountries.filter((c) => c.unMember == true);
 
 let id = null;
 
-let wonToday = false;
-let lostToday = false;
+let wonToday = 'false';
+let lostToday = 'false';
 
 let countryInfo = [];
 
@@ -36,14 +36,14 @@ function userWonContent() {
   status.textContent = "Congrats!";
   correctName.textContent = correctAnswer[1];
   correctFlag.src = correctAnswer[0];
-  localStorage.setItem("wonToday", true);
+  localStorage.setItem("wonToday", 'true');
 }
 
 function userLostContent() {
   status.textContent = "So Close...";
   correctName.textContent = correctAnswer[1];
   correctFlag.src = correctAnswer[0];
-  localStorage.setItem("lostToday", true);
+  localStorage.setItem("lostToday", 'true');
 }
 
 for (let i = 0; i < allCountries.length; i++) {
@@ -179,12 +179,12 @@ if (localStorage.getItem("inputDisabled") === "true") {
 }
 
 localStorage.getItem("wonToday") === "true"
-  ? (wonToday = true)
-  : (wonToday = false);
+  ? (wonToday = 'true')
+  : (wonToday = 'false');
 
 localStorage.getItem("lostToday") === "true"
-  ? (lostToday = true)
-  : (lostToday = false);
+  ? (lostToday = 'true')
+  : (lostToday = 'false');
 
 // Show/hide search results based on input value
 $("#input").on("input", function () {
@@ -333,12 +333,17 @@ howToPlay.addEventListener("click", () => {
 });
 backdropTutorial.addEventListener("click", () => {
   backdropTutorial.style.display = "none";
-}); //Show Coats Of Arms
+}); 
+//Show Coats Of Arms
 coatsOfArms.addEventListener("click", () => {
   backdropCoa.style.display = "flex";
 });
 backdropCoa.addEventListener("click", () => {
   backdropCoa.style.display = "none";
+});
+
+backdrop.addEventListener("click", () => {
+  backdrop.style.display = "none";
 });
 
 // Don;t show country list if input is not in focus
@@ -387,8 +392,8 @@ function clearStorageEveryMidnight() {
   localStorage.removeItem("guessNum");
   localStorage.removeItem("correctAnswer");
   localStorage.removeItem("inputDisabled");
-  localStorage.setItem("wonToday", false);
-  localStorage.setItem("lostToday", false);
+  localStorage.setItem("wonToday", 'false');
+  localStorage.setItem("lostToday", 'false');
 }
 
 setTimeout(() => {
@@ -434,13 +439,13 @@ setCountdown();
 let intervalId = setInterval(setCountdown, 1000);
 
 // Show the correct answer if the game has ended
-if (wonToday === true) {
+if (wonToday === 'true') {
   backdrop.style.display = "flex";
   status.textContent = "Congrats!";
   correctName.textContent = correctAnswer[1];
   correctFlag.src = correctAnswer[0];
 }
-if (lostToday === true) {
+if (lostToday === 'true') {
   backdrop.style.display = "flex";
   status.textContent = "So close...";
   correctName.textContent = correctAnswer[1];
