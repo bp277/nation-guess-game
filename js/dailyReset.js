f453f536c5b5e31cfac7f4e3c07aaa45f523014a
bp277 <bpejic27@gmail.com>
2023-06-06 12:27:35 +0200

    small daily reset fix

--- js/dailyReset.js
+++ js/dailyReset.js
@@ -2,10 +2,14 @@ function isFirstVisitToday() {
   const today = new Date().toLocaleDateString();
   let lastVisitDate = localStorage.getItem("lastVisitDate");
   if (!lastVisitDate) {
-    lastVisitDate = today;
+    lastVisitDate = document.cookie.replace(
+      /(?:(?:^|.*;\s*)lastVisitDate\s*\=\s*([^;]*).*$)|^.*$/,
+      "$1"
+    );
     localStorage.setItem("lastVisitDate", lastVisitDate);
   }
   if (lastVisitDate !== today) {
+    document.cookie = `lastVisitDate=${today}; max-age=86400; path=/`;
     localStorage.setItem("lastVisitDate", today);
     return true;
   }
